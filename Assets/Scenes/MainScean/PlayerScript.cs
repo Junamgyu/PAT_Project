00f933b1dd79a6111de3d4f07750dffa9955feed
Junamgyu <wnskarb13@naver.com>
2025-10-13 23:34:36 +0900

    player move, run and Rotation

--- Assets/Scenes/MainScean/PlayerScript.cs
+++ Assets/Scenes/MainScean/PlayerScript.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public float runSpeed;
+    public float rotationSpeed = 10f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,23 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float moveX = Input.GetAxisRaw("Horizontal");   //앞뒤 w일 경우 +1 s 일 경우 -1
+        float moveZ = Input.GetAxisRaw("Vertical");     //좌우 a일 경우 -1 d 일 경우 +1
+
+        Vector3 move = new Vector3(moveX, 0, moveZ).normalized;                 //이동 방향 벡터 계산
+        transform.Translate(move * runSpeed * Time.deltaTime, Space.World);     //이동처리
+
+        if (Input.GetKey(KeyCode.LeftShift)) runSpeed = 6f;                          // 달릴때 속도
+        else    runSpeed = 3f;                          // 걸을때 속도
+
+
+        //?플레이어 회전
+        if (move != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(move); //이동 방향을 바라보는 회전 계산
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+
         
     }
 }
