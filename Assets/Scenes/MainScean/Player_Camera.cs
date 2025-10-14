bd364457730bde79927dde7d655a004bf9f3653f
Junamgyu <wnskarb13@naver.com>
2025-10-15 00:46:07 +0900

    Modify to Camera
    Fix required: Character falling bug discovered

--- Assets/Scenes/MainScean/Player_Camera.cs
+++ Assets/Scenes/MainScean/Player_Camera.cs
@@ -7,8 +7,8 @@ public class Player_Camera : MonoBehaviour
     public Transform Target;
 
     [Header("Distance Setthing")]
-    public float Distance = 5.0f;
-    public float Height = 3.0f;
+    public float Distance = 3.0f;
+    public float Height = 1.0f;
 
     [Header("Max Rotation")]
     public float MouseSensitivity = 3.0f;   //마우스 회전 속도
@@ -17,8 +17,8 @@ public class Player_Camera : MonoBehaviour
    //마우스 입력 값;
     [Header("Smooth speed")]
     public float smoothSpeed = 10f;
-    private float mosY;     //마우스 좌우
-    private float mosX;     //마우스 위아래
+    private float mosY;     //마우스 위아래
+    private float mosX;     //마우스 좌우
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -33,10 +33,12 @@ public class Player_Camera : MonoBehaviour
         if (Target == null) return;
 
         //마우스 값 입력받기
-        mosX += Input.GetAxisRaw("Mouse X") * MouseSensitivity;
-        mosY -= Input.GetAxisRaw("Mouse Y") * MouseSensitivity;
+        // + 또는 - 하는 이유는 값이 계속 누적해야 되기 때문 각도유지를 위해 // = 만 하면 각도가 안움직이는 것 처럼 보임
 
-        //mosX = Mathf.Clamp(mosX, minYAngle, maxYAngle);     //상하 회전 제한
+        mosX += Input.GetAxisRaw("Mouse X") * MouseSensitivity;     //오른쪽 + 왼쪽 -   //오른쪽으로 돌리면 + 우측으로 돌아감
+        mosY -= Input.GetAxisRaw("Mouse Y") * MouseSensitivity;     //위 + 아래 -       //-=를 통해 방향 반대로 전환해주기
+
+        mosY = Mathf.Clamp(mosY, minYAngle, maxYAngle);     //상하 회전 제한
 
         Quaternion rotation = Quaternion.Euler(mosY, mosX, 0); //회전 계산
         Vector3 CameraPosition = Target.position - (rotation * Vector3.forward * Distance) + Vector3.up * Height; // 카메라가 있을 위치
